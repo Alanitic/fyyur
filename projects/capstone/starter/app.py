b985e04195828a240dc33d49027ca2b7ca1a127d
Alan Islas <alan.islas.itic@gmail.com>
2021-05-24 18:01:07 -0500

    Initial db config

--- projects/capstone/starter/app.py
+++ projects/capstone/starter/app.py
@@ -1,16 +1,19 @@
 import os
 from flask import Flask, request, abort, jsonify
-from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import setup_db
+
 
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  CORS(app)
+    # create and configure the app
+    app = Flask(__name__)
+    CORS(app)
+    setup_db(app)
 
-  return app
+    return app
+
 
 APP = create_app()
 
 if __name__ == '__main__':
-    APP.run(host='0.0.0.0', port=8080, debug=True)
+    APP.run(host='127.0.0.1', port=8080, debug=True)
--- projects/capstone/starter/models.py
+++ projects/capstone/starter/models.py
@@ -1,1 +1,51 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer
+from flask_migrate import Migrate
 
+database_name = "capstone"
+database_path = "postgres://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, db_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    Migrate(app, db)
+
+
+class Movie(db.Model):
+    id = Column(Integer, primary_key=True)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+class Actor(db.Model):
+    id = Column(Integer, primary_key=True)
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
